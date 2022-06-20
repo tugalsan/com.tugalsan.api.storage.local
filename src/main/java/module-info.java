acf81bad32ca5704fd6666f7f7d1092c437775c3
tugalsan karabacak <tugalsan@gmail.com>
2022-06-20 23:22:13 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.tugalsan.api.storage.local {
+    requires gwt.user;
+    requires com.tugalsan.api.executable;
+    requires com.tugalsan.api.thread;
+    requires com.tugalsan.api.log;
+    requires com.tugalsan.api.list;
+    exports com.tugalsan.api.storage.local.client;
+}
